519e0a601198ac818834d1981c0c2571d4521416
Adam J Lemmon <adamjlemmon@gmail.com>
2019-02-02 11:13:15 -0500

    resource added

--- product-registry-01/dev-stages/app-03.js
+++ product-registry-01/dev-stages/app-03.js
@@ -1,14 +1,23 @@
 class ProductRegistry extends React.Component {
-    render() {
-      return (
-        <div className='ui unstackable items'>
-          Hello, I am your first React component!
-        </div>
-      );
-    }
-  }  
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, I am your first React component!
+        <Product />
+      </div>
+    );
+  }
+}
 
-  ReactDOM.render(
-    <ProductRegistry />,
-    document.getElementById('content')
-  );
+class Product extends React.Component {
+  render() {
+    return (
+      <div>Hello I am a product.</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ProductRegistry />,
+  document.getElementById('content')
+);
