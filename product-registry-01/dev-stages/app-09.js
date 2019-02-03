341ea37c52052e3d2f6b9f079c50f27807c0f677
Adam J Lemmon <adamjlemmon@gmail.com>
2019-02-03 13:54:24 -0500

    resource added

---
+++ product-registry-01/dev-stages/app-09.js
@@ -0,0 +1,59 @@
+class ProductRegistry extends React.Component {
+  handleProductUpVote(productId) {
+    console.log(productId);
+  }
+
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        {
+          Seed.products.map(product => 
+            <Product
+              key={'product-'+product.id}
+              id={product.id}
+              title={product.title}
+              description={product.description}
+              submitterAvatarUrl={product.submitterAvatarUrl}
+              productImageUrl={product.productImageUrl}
+              votes={product.votes}
+              onVote={this.handleProductUpVote}
+            />
+          )
+        }
+      </div>
+    );
+  }
+}
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className='item'>
+        <div className='image'>
+          <img src={this.props.productImageUrl} />
+        </div>
+        <div className='middle aligned content'>
+          <div className='header'>
+            <a onClick={() => this.props.onVote(this.props.id)}>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
+          <div className='description'>
+            <a>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img className='ui avatar image' src={this.props.submitterAvatarUrl} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ProductRegistry />,
+  document.getElementById('content')
+);
